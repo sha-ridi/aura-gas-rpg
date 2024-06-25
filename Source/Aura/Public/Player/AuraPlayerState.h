e1e06f22215e54cfe8116ca800f5f85b18e2c06a
sha-ridi <sharipovadinag@gmail.com>
2024-06-25 13:11:03 +0400

    The Player State

---
+++ Source/Aura/Public/Player/AuraPlayerState.h
@@ -0,0 +1,20 @@
+// copyright sha-ridi
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "AuraPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+
+public:
+	AAuraPlayerState();
+	
+};
