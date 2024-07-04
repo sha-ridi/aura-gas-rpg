d0bf2318e861e3cd0763dce102c4072e1dd16801
sha-ridi <sharipovadinag@gmail.com>
2024-07-04 13:57:36 +0400

    Aura User Widget and Widget Controller

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,11 @@
+// copyright sha-ridi
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+    WidgetController = InWidgetController;
+    WidgetControllerSet();
+}
+
