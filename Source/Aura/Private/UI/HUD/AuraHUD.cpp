0829ddcee3fefde061fbfbab89cdf7a4b033b685
sha-ridi <sharipovadinag@gmail.com>
2024-07-09 15:38:09 +0400

    Listening for Attribute Changes

--- Source/Aura/Private/UI/HUD/AuraHUD.cpp
+++ Source/Aura/Private/UI/HUD/AuraHUD.cpp
@@ -29,7 +29,8 @@ UOverlayWidgetController* AAuraHUD::GetOverlayWidgetController(const FWidgetCont
     {
         OverlayWidgetController = NewObject<UOverlayWidgetController>(this, OverlayWidgetControllerClass);
         OverlayWidgetController->SetWidgetControllerParams(WCParams);
-
+        OverlayWidgetController->BindCallbacksToDependencies();
+        
         return OverlayWidgetController;
     }
     return OverlayWidgetController;
